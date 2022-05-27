d1a24669f94eb43c3aab92ce359fa5b119e35f3b
Likhith13 <97285676+Likhith-DK@users.noreply.github.com>
2022-05-27 11:10:17 +0530

    Working on:hello world

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World :");
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("The number is:" +i);
+        }
+    }
+}
